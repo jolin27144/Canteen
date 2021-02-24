1cb44ef9a755efffd2b67ff6c773deef2c999fe7
Sam Leung <liangrx8582@fxiaoke.com>
2021-02-24 18:02:57 +0800

    build: init

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import './app.css'
+
+function App(props) {
+  // this.props.children 是将要会渲染的页面
+  return props.children
+}
+
+export default App
---
+++ src/pages/index/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text } from '@tarojs/components';
+import { AtButton } from 'taro-ui';
+import 'taro-ui/dist/style/components/button.scss'; // 按需引入
+
+function Index() {
+  return (
+    <View className="index">
+      <Text>Hello world!</Text>
+      <AtButton type="primary">I need Taro UI</AtButton>
+      <Text>Taro UI 支持 Vue 了吗？</Text>
+      <AtButton type="primary" circle>
+        支持
+      </AtButton>
+      <Text>共建？</Text>
+      <AtButton type="secondary" circle>
+        来
+      </AtButton>
+    </View>
+  );
+}
+
+export default Index;
